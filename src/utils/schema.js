b2bacd6c578d361d8ee1d248afb4f82e7d1e312d
davincikab <davidnganganjeri079@gmail.com>
2021-02-20 10:40:08 +0300

    project structure and data schema

---
+++ src/utils/schema.js
@@ -0,0 +1,53 @@
+// Plot or Rental unit schema
+// []
+// aXpda
+//   plot_name = "Viva Complex"
+//   location = "Kayole, near Kasoko"
+//   type = "Storey Building"
+//   lat = 0.98789
+//   lng = 37.87778
+//   owner = "Mwangi Fauru"
+//   serivces[]
+//     electricity
+//       type = "token"
+//     water
+//     waste_management
+//       payment = "100"
+//     security[] 
+//   ammenities_nearby[] 
+//     schools[]
+//     health_facility[]
+//     market[0]
+//       name="kasoko"
+//       location="POINT ()"
+//     market[1]
+//       name="kasoko"
+//       location="POINT ()"
+//   property_types[] = Single,Double, One bedroom,two bedroom,
+
+// Houses schema
+// pKdfhf
+//   type="One bedroom
+//   cost="12000"
+//   is_occupied=true
+//   plot_id=aXpda
+
+// Tenants schema
+// pKdfhf
+//   id_number=35908766
+//   phone_number=0789765434
+//   name="Mwangi Mwas"
+//   unit_number=C4
+//   plot_name=Viva Complex
+//   house_type="One bedroom"
+//   is_active=true
+//   is_rent_paid=true,
+//   plot_id=aXpda
+
+// Rent payment Schema
+// zahA2d
+//   amount=12000
+//   date=2021-02-20T07:34:12.001Z
+//   tenant_id=pKdfhf
+//   plot_id=aXpda
+//   pending_balance=0
